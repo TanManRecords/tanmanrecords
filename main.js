5510eefd6cc15f0d31afabd9cd9496baafc3637c
TanManRecords <dakota@tanmanrecords.com>
2026-01-14 00:22:06 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Track data - exact display order for the wheel
 const tracks = [
-    { title: "DOGHAT - NATURAL HIGH (TMR006)", img: "images/SIPPIN.png", src: "music/SIPPIN.mp3" },
+    { title: "DOGHAT - NATURAL HIGH (TMR006)", img: "images/SIPPIN2.png", src: "music/SIPPIN.mp3" },
     { title: "HEADBOY - TRACK 6", img: "images/track6.png", src: "music/track 6.m4a" },
     { title: "DOGHAT - D.H.I.T.H", img: "images/D.H.I.T.H.png", src: "music/D.H.I.T.H.mp3" },
     { title: "12 HZUI - FISH CITY", img: "images/fish city.png", src: "music/fish city.m4a" },
