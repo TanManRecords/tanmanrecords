908ab22054769d68f96307ce86ffed0137e9769d
TanManRecords <dakota@tanmanrecords.com>
2026-01-19 19:48:12 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 const tracks = [
     { title: "DOGHAT - NATURAL HIGH (TMR006)", img: "images/SIPPIN2.png", src: "music/SIPPIN.mp3" },
     { title: "HEAD? - TELL ME TO SPANK ME (TMR003)", img: "images/head.png", src: "music/track 6.m4a" },
-    { title: "DOGHAT - D.H.I.T.H", img: "images/D.H.I.T.H.png", src: "music/D.H.I.T.H.mp3" },
+    { title: "DOGHAT - D.H.I.T.H", img: "images/indahouse.png", src: "music/D.H.I.T.H.mp3" },
     { title: "12 HZUI - FISH CITY", img: "images/fish city.png", src: "music/fish city.m4a" },
     { title: "WANKYWANKY", img: "images/wankywanky.png", src: "music/wankywanky.mp3" },
     { title: "STILLNESS", img: "images/stillness.png", src: "music/stillness.m4a" }
